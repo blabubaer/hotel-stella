11bf14e9e4423dfdc2a21756e6449c0fd1ab852f
Marten <mgransow@gmail.com>
2021-03-17 13:07:36 +0100

    all merged again

--- js/view.js
+++ js/view.js
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
-=======
 
 let app = document.getElementById("app");
 updateView();
->>>>>>> f038a168ca2d1055b9675e7de4a52d94f631abdb
 function updateView() {
     
     if (model.page.page_pos == 'home') {
         homepageview();
     } else if (model.page.page_pos == 'searchResults') {
         updateSearchView();
-<<<<<<< HEAD
-    }
-}
-
-function setSearchView() {
-    model.page.page_pos = 'searchResults';
-        console.log('test');
-    console.log(model.page.page_pos);
-    updateView();
-}
-
-=======
     }else if (model.page.page_pos == 'login'){
         updateLoginView();
     }
@@ -94,23 +79,18 @@ function homepageview() {
             `;
     app.innerHTML = html;
 }
->>>>>>> f038a168ca2d1055b9675e7de4a52d94f631abdb
 
 function updateSearchView() {
 
     let html = ``;
 
 
-<<<<<<< HEAD
-    html += '<div id="header"><h1>Hotell Stella</h1><span id="login" onclick="login()">login</span></div>';
-=======
     html += `<div id="header"><h1 onclick="setHomeView()">Hotell Stella - ${model.page.page_pos}</h1>`;
     if(model.page.current_user == 'guest'){
         html += `<span id="login" onclick="setLoginView()">login</span></div>`;
     }else{
         html += `<span id="login" onclick="userPanel()">${model.page.current_user}</span></div>`;
     }
->>>>>>> f038a168ca2d1055b9675e7de4a52d94f631abdb
     html += `
         <div class="searchField">    
             <label for="startDato">Start Dato:</label>
@@ -123,11 +103,7 @@ function updateSearchView() {
         </div>`;
         /* her må det skrives html */
     html +=  ` 
-<<<<<<< HEAD
-            <div class="searchResultWrapper"><img src="https://pix6.agoda.net/hotelImages/1198373/-1/f9583757bde8c4dec85d5fcc570ca86b.jpg?s=1024x768"> <h2>Dette er søkerresultat</h2> <p>søker resultat"</p></div>
-=======
             <p>Her kommer søkeresultat</p>
->>>>>>> f038a168ca2d1055b9675e7de4a52d94f631abdb
         `;
     html += `
         <div id="footer">
